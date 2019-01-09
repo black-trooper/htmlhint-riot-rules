b1388b379804587aa71b0e7f28e0bdf4e0a33980
black-trooper <hermanmiller.dkr@gmail.com>
2019-01-09 10:06:33 +0900

    First commit

---
+++ index.js
@@ -0,0 +1,28 @@
+'use strict'
+
+const glob = require('glob')
+const path = require('path')
+
+const defaultOptions = {
+  'avoid-tag-parent': true
+}
+
+function loadRule(filepath, options) {
+  options = Object.assign(defaultOptions, options)
+  filepath = path.resolve(filepath);
+  const ruleObj = require(filepath); // eslint-disable-line import/no-dynamic-require
+  const ruleOption = options[ruleObj.id]; // We can pass a value to the rule
+  return ruleOption ? ruleObj : null;
+}
+
+module.exports = function (options) {
+  const rules = 'node_modules/htmlhint-riot-rules/rules/*.js'
+  return glob.sync(rules, {
+    dot: false,
+    nodir: true,
+    strict: false,
+    silent: true
+  }).map(file => {
+    return loadRule(file, options)
+  }).filter(rule => rule !== null)
+}
---
+++ rules/avoid-tag-parent.js
@@ -0,0 +1,27 @@
+const id = 'avoid-tag-parent';
+
+module.exports = {
+  id,
+  description: 'Avoid tag.parent.',
+  init: function (parser, reporter) {
+    var self = this;
+    var regex = /\{.*parent.*?\}/;
+    parser.addListener('tagstart', function (event) {
+      var attrs = event.attrs,
+        attr,
+        col = event.col + event.tagName.length + 1;
+      for (var i = 0, l = attrs.length; i < l; i++) {
+        attr = attrs[i];
+
+        if (regex.test(attr.value) === true) {
+          reporter.warn('The attribute [ ' + attr.name + ' ] must avoid tag.parent.', event.line, col + attr.index, self, attr.raw);
+        }
+      }
+    });
+    parser.addListener('text', function (event) {
+      if (regex.test(event.raw) === true) {
+        reporter.warn('Must avoid tag.parent.', event.line, event.col, self, event.raw);
+      }
+    });
+  }
+}
