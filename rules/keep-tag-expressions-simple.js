0242b5a8d637cb0c61b786e49e3082eb57fe24c3
black-trooper <hermanmiller.dkr@gmail.com>
2019-01-20 23:13:59 +0900

    Add rule 'keep-tag-expressions-simple'

---
+++ rules/keep-tag-expressions-simple.js
@@ -0,0 +1,25 @@
+const id = 'keep-tag-expressions-simple';
+const esprima = require('esprima');
+
+module.exports = {
+  id,
+  description: 'Keep tag expressions simple',
+  init: function (parser, reporter) {
+    var self = this;
+    // RegExp Lookbehind Assertions. requires Node.js 9.11.2 or later
+    const regex = /(?<!\\){.*?[^\\]}/g;
+    parser.addListener('text', function (event) {
+      const expressions = event.raw.match(regex)
+      if (!expressions) {
+        return
+      }
+      expressions.forEach(expression => {
+        const code = expression.replace('\{', '').replace('\}', '')
+        const tokenSize = Object.keys(esprima.tokenize(code)).length
+        if (tokenSize > 10) {
+          reporter.warn('Must Keep tag expressions simple', event.line, event.col, self, event.raw);
+        }
+      })
+    });
+  }
+}
---
+++ test/rules/keep-tag-expressions-simple.spec.js
@@ -0,0 +1,49 @@
+var expect = require('expect.js')
+var HTMLHint = require('htmlhint').HTMLHint
+
+var ruleId = 'keep-tag-expressions-simple'
+var ruleOptions = {}
+ruleOptions[ruleId] = true
+HTMLHint.addRule(require(`../../rules/${ruleId}.js`))
+
+describe('Rules: ' + ruleId, function () {
+  it('Not simple expression should result in an error', function () {
+    var code = `<tag>
+      { (new Date()).getUTCFullYear() + '-' + ('0' + ((new Date()).getUTCMonth()+1)).slice(-2) }
+      { (new Date()).getUTCFullYear() + '-' + ('0' + ((new Date()).getUTCMonth()+1)).slice(-2) }
+    </tag>`
+    var messages = HTMLHint.verify(code, ruleOptions)
+    expect(messages.length).to.be(2)
+    expect(messages[0].rule.id).to.be(ruleId)
+    expect(messages[0].line).to.be(2)
+    expect(messages[0].col).to.be(1)
+    expect(messages[0].type).to.be('warning')
+
+    expect(messages[1].rule.id).to.be(ruleId)
+    expect(messages[1].line).to.be(2)
+    expect(messages[1].col).to.be(1)
+    expect(messages[1].type).to.be('warning')
+  })
+
+  it('Escaped expression should not result in an error', function () {
+    var code = `<tag>
+      <code>\\{ (new Date()).getUTCFullYear() + '-' + ('0' + ((new Date()).getUTCMonth()+1)).slice(-2) \\}</code>
+    </tag>`
+    var messages = HTMLHint.verify(code, ruleOptions)
+    expect(messages.length).to.be(0)
+  })
+
+  it('Simple expression should not result in an error', function () {
+    var code = `<tag>
+      { year() + '-' + month() }
+    </tag>`
+    var messages = HTMLHint.verify(code, ruleOptions)
+    expect(messages.length).to.be(0)
+  })
+
+  it('None expression should not result in an error', function () {
+    var code = `<tag>aaa</tag>`
+    var messages = HTMLHint.verify(code, ruleOptions)
+    expect(messages.length).to.be(0)
+  })
+})
