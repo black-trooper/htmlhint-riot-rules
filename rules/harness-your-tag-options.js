0cef69792deb80fa74c4fd0463ad6481806d9c95
black-trooper <hermanmiller.dkr@gmail.com>
2019-01-30 23:46:43 +0900

    Add rule 'harness-your-tag-options'

---
+++ rules/harness-your-tag-options.js
@@ -0,0 +1,42 @@
+const id = 'harness-your-tag-options'
+const esprima = require('esprima');
+
+module.exports = {
+  id,
+  description: 'Harness your tag options.',
+  init: function (parser, reporter, options) {
+    var self = this;
+    const regex = /\{.*opts.*?\}/;
+    function isProperty(body) {
+      return body.type === 'ExpressionStatement'
+        && body.expression.type === 'AssignmentExpression'
+        && body.expression.left.type === 'Identifier'
+    }
+    function extractRaw(raw, line) {
+      return raw.split(/\r\n|\r|\n/)[line - 1]
+    }
+    function warn(message, event, startLine, startCol) {
+      const line = event.line + startLine - 1
+      const col = (startLine === 1 ? event.col : 0) + startCol
+      const raw = extractRaw(event.raw, startLine)
+      reporter.warn(message, line, col, self, raw);
+    }
+    parser.addListener('tagstart', function (event) {
+      var attrs = event.attrs,
+        attr,
+        col = event.col + event.tagName.length + 1;
+      for (var i = 0, l = attrs.length; i < l; i++) {
+        attr = attrs[i];
+
+        if (regex.test(attr.value) === true) {
+          reporter.warn('Use defaults for option values. e.g.) tag.xxx = opts.xxx || \'defaults\'.', event.line, event.col, self, event.raw);
+        }
+      }
+    });
+    parser.addListener('text', function (event) {
+      if (regex.test(event.raw) === true) {
+        reporter.warn('Use defaults for option values. e.g.) tag.xxx = opts.xxx || \'defaults\'.', event.line, event.col, self, event.raw);
+      }
+    });
+  }
+}
