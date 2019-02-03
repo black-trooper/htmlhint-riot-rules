6c18975e3e7fbb86564c8a5d432c81a6e034496f
black-trooper <hermanmiller.dkr@gmail.com>
2019-02-03 21:04:59 +0900

    Add rule 'module-size'

---
+++ rules/module-size.js
@@ -0,0 +1,31 @@
+const id = 'module-size';
+
+module.exports = {
+  id,
+  description: 'Always construct your app out of small modules which do one thing and do it well.',
+  init: function (parser, reporter, options) {
+    const self = this;
+    let rootTag = '';
+    let limit = options > 1 ? options : 100
+
+    function onTagStart(event) {
+      var tagName = event.tagName.toLowerCase();
+      if (rootTag === '') {
+        rootTag = tagName;
+      }
+    }
+    function onTagEnd(event) {
+      var tagName = event.tagName.toLowerCase();
+      if (tagName === rootTag) {
+        if (limit < event.line) {
+          reporter.warn('Module size limit ', event.line, event.col, self, event.raw);
+        }
+        parser.removeListener('tagstart', onTagStart);
+        parser.removeListener('tagend', onTagEnd);
+      }
+    }
+
+    parser.addListener('tagstart', onTagStart);
+    parser.addListener('tagend', onTagEnd);
+  }
+}
