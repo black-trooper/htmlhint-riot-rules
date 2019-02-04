78c917d811bdcd6bf99c597cfbad360e53eb4886
black-trooper <hermanmiller.dkr@gmail.com>
2019-02-04 10:16:56 +0900

    Add test for 'module-size'

---
+++ test/rules/module-size.spec.js
@@ -0,0 +1,30 @@
+var expect = require('expect.js')
+var HTMLHint = require('htmlhint').HTMLHint
+
+var ruleId = 'module-size'
+var ruleOptions = {}
+ruleOptions[ruleId] = 6
+HTMLHint.addRule(require(`../../rules/${ruleId}.js`))
+
+describe('Rules: ' + ruleId, function () {
+  it('Module size limit over should result in an error', function () {
+    var code = `<tag><script>
+        const date = new Date()
+        this.message = "today is " + date
+        this.show = function(){
+          return this.message
+        }
+    </script></tag>`
+    var messages = HTMLHint.verify(code, ruleOptions)
+    expect(messages.length).to.be(1)
+    expect(messages[0].rule.id).to.be(ruleId)
+    expect(messages[0].line).to.be(7)
+    expect(messages[0].col).to.be(14)
+    expect(messages[0].type).to.be('warning')
+  })
+  it('Not exceed module size limit over should not result in an error', function () {
+    var code = '<tag><script></script></tag>'
+    var messages = HTMLHint.verify(code, ruleOptions)
+    expect(messages.length).to.be(0)
+  })
+})
