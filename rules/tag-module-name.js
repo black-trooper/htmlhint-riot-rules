de63e82f930273e1ee577935c5761dcd6a99ca0b
black-trooper <hermanmiller.dkr@gmail.com>
2019-02-05 11:06:53 +0900

    Add rule 'tag-module-name'

---
+++ rules/tag-module-name.js
@@ -0,0 +1,32 @@
+const id = 'tag-module-name';
+
+module.exports = {
+  id,
+  description: 'Custom element spec compliant. (include a hyphen, don\'t use reserved names.) \'app\'- namespaced',
+  init: function (parser, reporter, options) {
+    const self = this;
+    const regex = /-/;
+    let rootTag = '';
+
+    function onTagStart(event) {
+      var tagName = event.tagName.toLowerCase();
+      if (rootTag === '') {
+        rootTag = tagName;
+        if (!regex.test(rootTag)) {
+          reporter.error('Not custom element spec compliant. Include a hyphen.  \'app\'- namespaced.', event.line, event.col, self, event.raw);
+        }
+
+      }
+    }
+    function onTagEnd(event) {
+      var tagName = event.tagName.toLowerCase();
+      if (tagName === rootTag) {
+        parser.removeListener('tagstart', onTagStart);
+        parser.removeListener('tagend', onTagEnd);
+      }
+    }
+
+    parser.addListener('tagstart', onTagStart);
+    parser.addListener('tagend', onTagEnd);
+  }
+}
