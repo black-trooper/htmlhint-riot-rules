8f50b9eaf170587c84caba62077b7725a8bf10c9
black-trooper <hermanmiller.dkr@gmail.com>
2019-03-13 22:36:23 +0900

    Fix the problem when has 'each' attribute

--- test/rules/tag-parent-disabled.spec.js
+++ test/rules/tag-parent-disabled.spec.js
@@ -28,19 +28,13 @@ describe('Rules: ' + ruleId, function () {
   })
 
   it('Use parent.parent attribute value with each attribute should result in an error', function () {
-    var code = '<tag><p each="{ item in items }" on-click="{ parent.parent.show }"></p></tag>'
+    var code = '<tag><p each="{ item in items }" on-click="{ parent.show }"></p></tag>'
     var messages = HTMLHint.verify(code, ruleOptions)
     expect(messages.length).to.be(1)
     expect(messages[0].rule.id).to.be(ruleId)
     expect(messages[0].line).to.be(1)
     expect(messages[0].col).to.be(33)
     expect(messages[0].type).to.be('warning')
-  })
-
-  it('Use parent attribute value with each attribute should not result in an error', function () {
-    var code = '<tag><p each="{ item in items }" on-click="{ parent.show }"></p></tag>'
-    var messages = HTMLHint.verify(code, ruleOptions)
-    expect(messages.length).to.be(0)
   })
 
   it('None parent attribute value should not result in an error', function () {
